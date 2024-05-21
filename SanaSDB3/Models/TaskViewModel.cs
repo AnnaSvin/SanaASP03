af984da96f1e57cedb9bee6ac66a26e010329a50
Anna Svintsitska <ipz233_saye@student.ztu.edu.ua>
2024-05-21 13:39:07 +0300

    Made ToDoList

---
+++ SanaSDB3/Models/TaskViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SanaSDB3.ViewModels
+{
+    public class TaskViewModel
+    {
+        public IEnumerable<Tasks> TaskList { get; set; } = new List<Tasks>();
+        public Tasks NewTask { get; set; }
+    }
+}
